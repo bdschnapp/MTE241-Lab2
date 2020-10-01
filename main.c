95323c60be3129e2cc8c67b63d4a0db601c0fc43
Benjamin Schnapp <bdschnapp@gmail.com>
2020-10-01 17:48:18 -0400

    Added logic for program 1 and 3 TODO: test and debug, as well as verify peripheral addresses TODO: add pause functionality for program 3

--- main.c
+++ main.c
@@ -36,19 +36,41 @@ const char * base10_to_base2(int base10){
 }
 
 int main(){
-    //TODO: Program 1
+    //Program 1
+    //TODO: test and debug
     #ifdef a
-    //declare constants and I/O locations
+    //Declare variables and I/O locations
+    int previous = 0;
+    int input = 0;
+    unsigned int *p_input;
+    p_input = 0x3042FD71;
+    LPC_GPIO2->FIODIR &= ~(1<<10); //Push button in input mode
+    LPC_GPIO1->FIODIR |= (1<<28); //LED in output mode
+
+    //Main loop
     while(1){
         //read from inputs
+        input = (LPC_GPIO1->FIOPIN>>0) & 0x01;
 
         //write to LEDs
+        if ((previous == 0) && (input == 1)){
+            //turn on
+            LPC_GPIO1->FIOSET |= (1<<28);
+            previous = 1;
+        }
+        else if((previous == 1) && (input == 0)){
+            //turn off
+            LPC_GPIO1->FIOCLR |= (1<<28);
+            previous = 0;
+        }
     }
     #endif
 
     //TODO: Program 2
     #ifdef b
     //declare constants and I/O locations
+
+    //Main loop
     while(1){
         //read from inputs
 
@@ -63,21 +85,36 @@ int main(){
 
     //TODO: Program 3
     #ifdef c
-    //declare constants and I/O locations
+    //declare variables and I/O locations
+    char str_input[5];
+    char str_output[9];
+    int int_input = 0;
+
+    //Main loop
     while(1){
+        //Clear any values from previous loops
+        sprintf(str_input, "");
+        sprintf(str_output, "");
+
         //read user input
+        scanf("%s", str_input);
 
         //convert input to a base10 int
+        int_input = atoi(str_input);
 
         //convert that int to a base2 string and print
+        str_output = base10_tobase_2(int_input);
+        printf(str_output);
 
-        //pause for readability
+        //TODO: pause for readability
     }
     #endif
 
     //TODO: Program 4
     #ifdef d
     // declare constants and I/O locations and turn on peripherials
+
+    //Main loop
     while(1){
         //read input
 
